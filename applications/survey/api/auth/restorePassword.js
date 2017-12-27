4e93818ce4b483982692ad1b39b716a0369238a5
Mykola Bilochub <nbelochub@gmail.com>
2017-12-28 01:45:56 +0200

    Implement `auth.restorePassword` method

--- applications/survey/api/auth/restorePassword.js
+++ applications/survey/api/auth/restorePassword.js
@@ -3,5 +3,60 @@
     callback(api.jstp.ERR_INVALID_SIGNATURE);
     return;
   }
-  callback();
+
+  gs.connection.select({
+    email,
+    category: 'students',
+  }).fetch((err, res) => {
+    if (err) {
+      application.log.error(
+        `In auth.restorePassword restorePassword: ${err}`
+      );
+      callback(api.jstp.ERR_INTERNAL_API_ERROR);
+      return;
+    }
+
+    if (res.length === 0) {
+      callback(api.auth.errors.ERR_INVALID_CREDENTIALS);
+      return;
+    }
+
+    generateRestorationCode(res[0]);
+  });
+
+  function generateRestorationCode(student) {
+    const code = api.common.generateKey(32, api.common.ALPHA_DIGIT);
+    student.restoration = code;
+
+    sendEmail(code);
+    updateStudent(student);
+  }
+
+  function sendEmail(restorationCode) {
+    api.mail.sendMail({
+      from: application.config.restorationMail.from,
+      to: email,
+      subject: application.config.restorationMail.subject,
+      text: restorationCode,
+    }, (err) => {
+      if (err) {
+        application.log.error(
+          `In auth.restorePassword sendEmail: ${err}`
+        );
+      }
+    });
+  }
+
+  function updateStudent(student) {
+    gs.connection.update(student, (err) => {
+      if (err) {
+        application.log.error(
+          `In auth.restorePassword updateStudent: ${err}`
+        );
+        callback(api.jstp.ERR_INTERNAL_API_ERROR);
+        return;
+      }
+      callback();
+    });
+  }
 }
