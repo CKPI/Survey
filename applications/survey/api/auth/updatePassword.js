dc2c732b57a96875131489e4cd330ff6f62f70e9
Mykola Bilochub <nbelochub@gmail.com>
2017-12-28 02:04:51 +0200

    Add `auth.updatePassword` method implementation

--- applications/survey/api/auth/updatePassword.js
+++ applications/survey/api/auth/updatePassword.js
@@ -3,5 +3,55 @@
     callback(api.jstp.ERR_INVALID_SIGNATURE);
     return;
   }
-  callback();
+
+  gs.connection.select({
+    restoration: token,
+    category: 'students',
+  }).fetch((err, res) => {
+    if (err) {
+      application.log.error(
+        `In auth.updatePassword gs.select: ${err}`
+      );
+      callback(api.jstp.ERR_INTERNAL_API_ERROR);
+      return;
+    }
+
+    if (res.length === 0) {
+      callback(api.auth.errors.ERR_INVALID_TOKEN);
+      return;
+    }
+
+    hashPassword(res[0]);
+  });
+
+  function hashPassword(student) {
+    api.auth.hash(password, (err, hash) => {
+      if (err) {
+        application.log.error(
+          `In auth.updatePassword hashPassword: ${err}`
+        );
+        callback(api.jstp.ERR_INTERNAL_API_ERROR);
+        return;
+      }
+
+      student.password = hash;
+
+      delete student.restoration;
+
+      updateStudent(student);
+    });
+  }
+
+  function updateStudent(student) {
+    gs.connection.update(student, (err) => {
+      if (err) {
+        application.log.error(
+          `In auth.updatePassword updateStudent: ${err}`
+        );
+        callback(api.jstp.ERR_INTERNAL_API_ERROR);
+        return;
+      }
+      callback();
+    });
+  }
 }
