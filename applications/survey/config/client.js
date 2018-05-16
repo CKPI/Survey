f1d3d2fe21e91823f0bb70876b30fa3d7d5f29b8
Dmytro Nechai <nechaido@gmail.com>
2018-05-17 00:09:19 +0300

    Change localisations

--- applications/survey/config/client.js
+++ applications/survey/config/client.js
@@ -10,5 +10,13 @@
       studentCardNumber: 'Enter your students card number',
       studentCardSeries: 'Enter your students card series',
     },
+    ua: {
+      ipn: 'Введіть ІПН',
+      passportSeries: 'Введіть серію паспорта',
+      passportNumber: 'Введіть номер паспорта',
+      passportCreationDate: 'Введіть дату видачі паспорта',
+      studentCardNumber: 'Введіть серію студентського квитка',
+      studentCardSeries: 'Введіть номер студентського квитка',
+    },
   },
 }
